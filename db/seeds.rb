379c0ec2b334a751121a6b48a891255f8efdd804
Rayane Nordine <rayanenordine@gmail.com>
2022-04-06 11:56:38 +0200

    basic seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all messages"
+Message.destroy_all
+puts "Destroying all chatrooms"
+Chatroom.destroy_all
+puts "Destroying all users"
+User.destroy_all
+puts "DESTROYING THE WOOOOOORLD"
+puts "lol"
+puts "Creating users"
+
+User.create!(email: "user1@gmail.com", password: "secret", first_name: "Michel", language_spoken: "French",
+            language_learnt: "Spanish")
+puts "Michel speaking French and wanting to learn Spanish created"
+User.create!(email: "user2@gmail.com", password: "secret", first_name: "Pedro", language_spoken: "Spanish",
+            language_learnt: "French")
+puts "Pedro speaking Spanish and wanting to learn French created"
+puts "Creating Chatroom"
+Chatroom.create!(user_one:User.first.id, user_two:User.last.id)
+
+puts "Chatroom created"
+puts "You can find the chatroom at /chatrooms/#{Chatroom.first.id}"
